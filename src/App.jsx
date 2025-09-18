560e5f31226dbcb4eec62f3f8c6cdbfa99d1d064
Hampus von Horn <hampus.vh@gmail.com>
2025-09-18 13:37:42 +0200

    Add missing template parts + experimental view

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+      <div className="masthead">
+        <header className="topbar">
+          <div className="container">
+            <strong></strong>
+          </div>
+        </header>
+
+        <section className="hero">
+          <div className="container">
+
+          </div>
+          <div className="hero-fade" />
+        </section>
+      </div>
+
+      <main className="container">
+        <section id="about" className="section">About</section>
+        <section id="projects" className="section"></section>
+        <section id="contact" className="section">Contact</section>
+      </main>
+
+      <footer className="footer">© {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
-export default App
+export default App;
